75fd3dc155075add503de7ca5de819953afa1751
Kyu Park <park@kyu.co>
2012-08-30 15:14:03 -0400

    dday

--- app.rb
+++ app.rb
@@ -2,24 +2,10 @@ require 'rubygems'
 require 'sinatra'
 
 get '/' do
-  @title = "Home Page"
+  @title = "dday"
   haml :index
 end
 
-get '/about' do
-  @title = "About Us"
-  haml :about
-end
-
-get '/contact' do
-  @title = "Contact Us"
-  haml :contact
-end
-
-get '/reverse/:string' do
-  params[:string].reverse
-end
-
-get '/upcase/:string' do
-  params[:string].upcase
+get '/:year/:mon/:day' do
+  
 end
