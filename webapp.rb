b89e8dc7914fafc3b67cedf68bb4c81ff4acebef
Kyu Park <park@kyu.co>
2012-09-05 12:47:09 -0400

    fix css

--- webapp.rb
+++ webapp.rb
@@ -5,23 +5,20 @@ require 'dday'
 
 class DDayApp < Sinatra::Base
 
-  def start
-    @d = DDay.new(@year, @month, @day)
-    @today = @d.set_today
-    @title = @string
-    slim @d.template
-  end
-  
   def getURL
-    @string = params[:string]
     @year  = params[:year].to_i
     @month = params[:month].to_i
     @day   = params[:day].to_i
+    @string = params[:string]
+  end
+  
+  def start
+    @d = DDay.new(@year, @month, @day)
+    @today = @d.set_today
+    slim @d.template
   end
   
   get '/' do
-    @string = "Chrsitmas"
-    params[:string] = @string
     slim :index
   end
 
