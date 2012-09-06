aeb328b171d3fbe98201c2de4d3c9a1855c250ee
Kyu Park <park@kyu.co>
2012-09-05 21:11:26 -0400

    ordinal function and css

--- lib/dday.rb
+++ lib/dday.rb
@@ -1,3 +1,21 @@
+def ordinal value
+ 
+    case value.to_s
+    when /^[0-9]*[1][0-9]$/
+      suffix = "th"
+    when /^[0-9]*[1]$/
+      suffix = "st"
+    when /^[0-9]*[2]$/
+      suffix = "nd"
+    when /^[0-9]*[3]$/
+      suffix = "rd"
+    else
+      suffix = "th"
+    end
+ 
+    return suffix
+end
+
 def days_before(dday, days) 
   date = dday - days
   DDay.new(date.year, date.month, date.day)
